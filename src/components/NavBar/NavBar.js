7243bae8d2c4157449980b0c8216ef5d249c9a2f
MoeShoe <sahlimoussa20@gmail.com>
2022-07-28 14:25:25 +0100

    style improvements

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -8,27 +8,27 @@ class NavBar extends Component {
   render() {
     return (
       <div className={styles["nav-bar-container"]}>
+        {/*TODO, actually.. i'll do just that! * I thought about making categories render dynamically from the API but i
+        decided against it to avoid over-engineering */}
         <div
           className={`${styles["container"]} ${styles["categories-container"]}`}
         >
           <div className={`${styles["category"]} ${styles["category-active"]}`}>
-            WOMEN
+            ALL
           </div>
-          <div className={styles["category"]}>MEN</div>
-          <div className={styles["category"]}>KIDS</div>
+          <div className={styles["category"]}>CLOTHES</div>
+          <div className={styles["category"]}>TECH</div>
         </div>
-
         <div
           className={`${styles["container"]} ${styles["web-app-logo-container"]}`}
         >
           <Logo />
         </div>
-
         <div
           className={`${styles["container"]} ${styles["actions-container"]}`}
         >
           <div className={styles["action"]}>
-            $ <span style={{}}>&#8964;</span>
+            $ <span>&#8964;</span>
           </div>
           <div className={styles["action"]}>
             <Cart />
