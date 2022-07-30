ab25960c79b67e9c510d2e46bb711ee4d3669928
MoeShoe <sahlimoussa20@gmail.com>
2022-07-30 17:16:35 +0100

    add indiania-drag-scroll packaga and improvements

--- src/components/ProductDesc/ProductImages.js
+++ src/components/ProductDesc/ProductImages.js
@@ -1,21 +1,36 @@
 import { Component } from "react";
+import ScrollContainer from "react-indiana-drag-scroll";
 
 import styles from "./ProductImages.module.css";
 
 class ProductImages extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { currentImage: this.props?.imagesList?.at(0) };
+  }
+
+  imageSelectClickHandler(e) {
+    this.setState({ currentImage: e.target.src });
+  }
+
   render() {
     return (
       <>
         <div className={styles["images-container"]}>
           {this.props.imagesList?.map((img, i) => (
             <div className={styles["image-select-container"]}>
-              <img src={img} alt={`product ${i}`} key={i} />
+              <img
+                src={img}
+                alt={`product ${i}`}
+                key={i}
+                onClick={this.imageSelectClickHandler.bind(this)}
+              />
             </div>
           ))}
         </div>
-        <div className={styles["main-image-container"]}>
-          <img src={this.props.imagesList?.at(0)} alt="product" />
-        </div>
+        <ScrollContainer className={styles["main-image-container"]}>
+          <img src={this.state.currentImage} alt="product" />
+        </ScrollContainer>
       </>
     );
   }
