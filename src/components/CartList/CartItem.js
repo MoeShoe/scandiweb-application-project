ab2e7754043f6cb4e978b1824ed28fd65ec6e509
MoeShoe <sahlimoussa20@gmail.com>
2022-07-31 05:14:12 +0100

    creata CartList and CartItem component

---
+++ src/components/CartList/CartItem.js
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+import styles from "./CartItem.module.css";
+
+class CartItem extends Component {
+  render() {
+    return <div>This is the cart item component !</div>;
+  }
+}
+
+export default CartItem;
---
+++ src/components/CartList/CartList.js
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+import styles from "./CartList.module.css";
+
+class CartList extends Component {
+  render() {
+    return <div>This is the cart list!</div>;
+  }
+}
+
+export default CartList;
